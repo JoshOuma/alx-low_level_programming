c3966de90817c85f1dabbcb601739d893b010b2f
JoshOuma <JoshOuma>
2023-07-18 15:15:25 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,58 +1,24 @@
 #include <stdio.h>
-
-
 /**
-
- * main - finds and prints the sum of the even-valued terms
-
- * followed by a new line
-
- * Return: Always 0 (Success)
-
+ *main - print first 50 fibonacci
+ *
+ *Return: 0 always.
  */
-
 int main(void)
-
 {
+	long int i, x = 1, y = 2, sum = 0, tSum = 0;
 
-        int i;
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			tSum = tSum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
 
-        unsigned long int j, k, next, sum;
-
-
-        j = 1;
-
-        k = 2;
-
-        sum = 0;
-
-
-        for (i = 1; i <= 33; ++i)
-
-        {
-
-                if (j < 4000000 && (j % 2) == 0)
-
-                {
-
-                        sum = sum + j;
-
-                }
-
-                next = j + k;
-
-                j = k;
-
-                k = next;
-
-        }
-
-
-        printf("%lu\n", sum);
-
-
-        return (0);
-
+	}
+	printf("%ld\n", tSum);
+	return (0);
 }
-
-
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,41 +1,14 @@
 #include "main.h"
-
-
 /**
-
- * print_last_digit - prints the last digit of a number
-
- * @n: the int to extract the last digit from
-
- * Return: value of the last digit
-
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to be computed
+ *
+ * Return: The absolute value of the integer
  */
-
-int print_last_digit(int n)
-
+int _abs(int n)
 {
-
-        int a;
-
-
-        if (n < 0)
-
-        n = -n;
-
-
-        a = n % 10;
-
-
-        if (a < 0)
-
-                a = -a;
-
-
-        _putchar(a + '0');
-
-
-        return (a);
-
+if (n >= 0)
+return (n);
+else
+return (-n);
 }
-
-
